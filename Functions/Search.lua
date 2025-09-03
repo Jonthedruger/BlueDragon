a0bd2bc8f5fea5afd25021756d660db1bcd6b950
liebertmotion <137779989+Jonthedruger@users.noreply.github.com>
2025-09-03 20:23:01 +0700

    Create Search.lua

---
+++ Functions/Search.lua
@@ -0,0 +1,17 @@
+BYTE          = gg.TYPE_BYTE        -- 1 byte integer
+WORD          = gg.TYPE_WORD        -- 2 byte integer
+DWORD         = gg.TYPE_DWORD       -- 4 byte integer
+QWORD         = gg.TYPE_QWORD       -- 8 byte integer
+FLOAT         = gg.TYPE_FLOAT       -- 4 byte float
+DOUBLE        = gg.TYPE_DOUBLE      -- 8 byte double
+SIGNED_BYTE   = gg.TYPE_SIGNED_BYTE   -- signed 1 byte
+SIGNED_WORD   = gg.TYPE_SIGNED_WORD   -- signed 2 byte
+SIGNED_DWORD  = gg.TYPE_SIGNED_DWORD  -- signed 4 byte
+SIGNED_QWORD  = gg.TYPE_SIGNED_QWORD  -- signed 8 byte
+EQUAL         = gg.SIGN_EQUAL         -- equal
+AUTO          = gg.TYPE_AUTO        -- auto
+
+function Search(...)
+  local Args = {...}
+  gg.searchNumber(Args[1],Args[2] or AUTO,
+  
