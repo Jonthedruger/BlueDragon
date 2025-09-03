75f6871c8ea13d3978640edb473591ac875d407d
liebertmotion <137779989+Jonthedruger@users.noreply.github.com>
2025-09-03 20:44:37 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,39 @@
 local Memory  = require('./Functions/Memory.lua')
 local Values  = require('./Modules/Values.lua')
 local Version = require('./Modules/Version')
+
+local function Menu()
+  local Menu = {
+    'Sniper Aimbot';
+    'Fast Switch';
+    'Antenna';
+  }
+  local Args = gg.multiChoice(Menu, nil, [==[
+    - Information
+    This script was maded by liebertsx. Thanks for using my script
+ 
+    Discord: @liebertsx
+    Telegram: @liebertsx
+  ]==])
+  if not Args then return end -- this shi lame
+  if Args[1] then
+    Memory.Search(Values['Sniper Aimbot'].S, FLOAT)
+    Memory.Edit(Values['Sniper Aimbot'].R, BYTE)
+  elseif Args[2] then
+    Memory.Search(Values['Fast Switch'].S, BYTE)
+    Memory.Edit(Values['Fast Switch'].R, BYTE)
+    Memory.Search(Values['Fast Switch'].S2, BYTE)
+    Memory.Edit(Values['Fast Switch'].R2, BYTE)
+  elseif Args[3] then
+    Memory.Search(Values.Antenna.S, FLOAT)
+    Memory.Edit(Values.Atenna.R, FLOAT)
+  end
+end
+
+while true do
+  gg.sleep(100)
+  if gg.isVisible() then
+    gg.setVisible(false)
+    Menu()
+  end
+end
