f26876c9c954f685092cd2fc9a005123ddd20175
liebertmotion <137779989+Jonthedruger@users.noreply.github.com>
2025-09-04 15:56:24 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -21,16 +21,16 @@ Telegram: @liebertsx
     
     if not Args then return end
     if Args[1] then
-        Memory.Search(Values['Sniper Aimbot'].S, Memory.FLOAT)
-        Memory.Edit(Values['Sniper Aimbot'].R, Memory.FLOAT)
+        Memory.Search(Values['Sniper Aimbot'].S, Memory.Float)
+        Memory.Edit(Values['Sniper Aimbot'].R, Memory.Byte)
     elseif Args[2] then
         Memory.Search(Values['Fast Switch'].S, Memory.BYTE)
-        Memory.Edit(Values['Fast Switch'].R, Memory.BYTE)
-        Memory.Search(Values['Fast Switch'].S2, Memory.BYTE)
-        Memory.Edit(Values['Fast Switch'].R2, Memory.BYTE)
+        Memory.Edit(Values['Fast Switch'].R, Memory.Byte)
+        Memory.Search(Values['Fast Switch'].S2, Memory.Byte)
+        Memory.Edit(Values['Fast Switch'].R2, Memory.Byte)
     elseif Args[3] then
-        Memory.Search(Values.Antenna.S, Memory.FLOAT)
-        Memory.Edit(Values.Antenna.R, Memory.FLOAT)
+        Memory.Search(Values.Antenna.S, Memory.Float)
+        Memory.Edit(Values.Antenna.R, Memory.Float)
     end
 end
 
