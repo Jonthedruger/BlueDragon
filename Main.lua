a2d66f088bf22853e76e35015cfad4965e20226b
liebertmotion <137779989+Jonthedruger@users.noreply.github.com>
2025-09-04 19:20:02 +0700

    fixed antenna again
    added something

--- Main.lua
+++ Main.lua
@@ -3,6 +3,7 @@ local Main = Repo .. '/main/'
 local Memory = load(gg.makeRequest(Main .. 'Functions/Memory.lua').content)()
 local Values = load(gg.makeRequest(Main .. 'Modules/Values.lua').content)()
 local Version = load(gg.makeRequest(Main .. 'Modules/Version.lua').content)()
+Memory.SetRanges(Memory.Anonymous | Memory.CodeApp)
 
 local function Menu()
     local Menu = {
@@ -31,18 +32,21 @@ Telegram: @liebertsx
     elseif Args[3] then
         Memory.Search(Values.Antenna.S, Memory.Float)
         Memory.Search(0, Memory.Float)
+        local Results = Memory.GetResults()
         for i,v in ipairs(Results) do
             v.value = Values.Antenna.R
             v.freeze = true
         end
-        gg.setValues(Results)
+        Memory.SetValues(Results)
+        Memory.Clear()
         Memory.Search(Values.Antenna.S2, Memory.Float)
         Memory.Search(0, Memory.Float)
-        for k,x in ipairs(Results) do
+        local Results2 = Memory.GetResults()
+        for k,x in ipairs(Results2) do
             x.value = Values.Antenna.R
             v.freeze = true
         end
-        gg.setValues(Results)
+        Memory.SetValues(Results2)
         Memory.Clear()
     end
 end
