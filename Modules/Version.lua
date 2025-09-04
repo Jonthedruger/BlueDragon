38a94e7f18195b5758a302b7891471b269605de9
liebertmotion <137779989+Jonthedruger@users.noreply.github.com>
2025-09-04 19:25:19 +0700

    Update Version.lua

--- Modules/Version.lua
+++ Modules/Version.lua
@@ -2,4 +2,5 @@ Changelogs = {
   ['1.0.0'] = 'Script released.',
   ['1.0.1'] = '- Wednesday 3/9/2025\nAdded Memory.Patch in Memory',
   ['1.0.2'] = '- Thursday 4/9/2025\nFixed Minor/Major Bugs & Issues\nUpdated Values,Versions & Memory'
-return '1.0.2';
+}
+return Changelogs;
