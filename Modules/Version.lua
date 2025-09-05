53e5c4dbc144352c1ef643935325f5a06d4b94e6
liebertmotion <137779989+Jonthedruger@users.noreply.github.com>
2025-09-05 13:20:17 +0700

    Update Version.lua

--- Modules/Version.lua
+++ Modules/Version.lua
@@ -1,6 +1,7 @@
 Changelogs = {
   ['1.0.0'] = 'Script released.',
-  ['1.0.1'] = '- Wednesday 3/9/2025\nAdded Memory.Patch in Memory',
-  ['1.0.2'] = '- Thursday 4/9/2025\nFixed Minor/Major Bugs & Issues\nUpdated Values,Versions & Memory'
+  ['1.0.1'] = '- Wednesday 3/09/2025\nAdded Memory.Patch in Memory',
+  ['1.0.2'] = '- Thursday 4/09/2025\nFixed Minor/Major Bugs & Issues\nUpdated Values,Versions & Memory',
+  ['1.0.3'] = '- Friday 5/09/2025\nNew Sniper Aimbot\nFixed Antenna\nFixed Minor Bugs"
 }
 return Changelogs;
