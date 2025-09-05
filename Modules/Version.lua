cc4e6e06c1709ab3a9e43d593a70de59ddb3ee3b
liebertmotion <137779989+Jonthedruger@users.noreply.github.com>
2025-09-05 18:14:18 +0700

    Delete Modules/Version.lua

--- Modules/Version.lua
+++
@@ -1,9 +0,0 @@
-Changelogs  = {
-  ['1.0.0'] = '- Script released.';
-  ['1.0.1'] = '- Wednesday 3/09/2025\nAdded Memory.Patch in Memory';
-  ['1.0.2'] = '- Thursday 4/09/2025\nFixed Minor/Major Bugs & Issues\nUpdated Values,Versions & Memory';
-  ['1.0.3'] = '- Friday 5/09/2025\nNew Sniper Aimbot\nFixed Antenna\nFixed Minor Bugs";
-  ['1.0.4'] = '- Friday 5/09/2025\nUpdated & Fixed Fast Switch\nAdded AntiCheat/Bypass (Still In Beta)';
-}
-
-return Changelogs; -- mustard
